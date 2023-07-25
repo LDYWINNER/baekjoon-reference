0115915cbb90f2b896867c948b9f7deb7615c118
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-07-26 02:20:17 +0900

    그리디 - 문자열 뒤집기

--- greedy/exercise/곱하기 혹은 더하기 answer.py
+++ greedy/exercise/곱하기 혹은 더하기 answer.py
@@ -4,6 +4,7 @@ data = input()
 result = int(data[0])
 
 for i in range(1, len(data)):
+    # 두 수 중에서 하나라도 0 혹은 1인 경우, 곱하기말고 더하기 수행
     num = int(data[i])
     if num <= 1 or result <= 1:
         result += num
