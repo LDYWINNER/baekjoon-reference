6874b989125e83c0c1d6c8493854e3033afe3fd1
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-08-10 21:35:53 +0900

    DS - queue

---
+++ DataStructure/queue.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+# 큐(Queue) 구현을 위해 deque 라이브러리 사용
+queue = deque()
+
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue) # 먼저 들어온 순서대로 출력
+queue.reverse() # 다음 출력을 위해 역순으로 바꾸기
+print(queue) # 나중에 들어온 원소부터 출력
+
+
+# python으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용함
+# deque는 스택과 큐의 장점을 모두 채택한 것인데 데이터를 넣고 빼는 속도가 리스트 자료형에 비해 효율적이며 queue 라이브러리를 이용하는 것보다 간단함
+# 더불어 대부분의 코딩 테스트에서는 collections 모듈과 같은 기본 라이브버리 사용을 허용하므로 안심하고 사용해도 괜찮음
