7d406ea23bcac88adaab0cc8fe37ee318b2647f1
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-08-11 13:25:24 +0900

    DS - graph - adjacency matrix

--- basic_algorithm/factorial.py
+++ basic_algorithm/factorial.py
@@ -7,7 +7,7 @@ def factorial_iterative(n):
     return result
 
 # 재귀적으로 구현한 n!
-def factorial_recursive(n):        
+def factorial_recursive(n):
     if n <= 1: # n이 1 이하인 경우 1을 반환
         return 1
     # n! = n * (n - 1)!를 그대로 코드로 작성하기
